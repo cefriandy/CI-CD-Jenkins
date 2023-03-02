8cfc23aa194b39909226180bbc8b6614c9519215
cefriandy <cefriandysimarmata@gmail.com>
2023-03-02 09:46:35 +0700

    add greeting api

--- src/main/java/com/cicd/CicdApplication.java
+++ src/main/java/com/cicd/CicdApplication.java
@@ -2,9 +2,18 @@ package com.cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CicdApplication {
+
+
+	@GetMapping(value = "/greetings")
+	public String welcome(){
+		return "this is for jenkins test porpose";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CicdApplication.class, args);
